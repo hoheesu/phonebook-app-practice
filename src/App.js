1888e0e1a46c54fbdb93ac4ceb4c7ccdcb605432
hoheesu <hoheesu@naver.com>
2023-01-15 23:30:03 +0900

    feat: create ContactForm
    React-BootStrap을 연결하고 연락처 입력 폼을 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import ContactForm  from './conponents/ContactForm'
+
+// 1. 왼쪽에 연락처 등록하는 폼이 있고 오른쪽에는 연락처 리스트와 search 창이 있다.
+// 2. 리스트에 유저 이름과 전화번호를 추가 할 수 있다.
+// 3. 리스트에 아이템이 몇개 있는지 보인다.
+// 4. 사용자가 유저를 이름검색으로 찾을 수 잇다.
+
 
 function App() {
   return (
-    <div>
-     
-    </div>
+     <Container>
+       <h1 className='title'>연락처</h1>
+        <Row>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col>
+          </Col>
+        </Row>
+     </Container>
   );
 }
 
---
+++ src/conponents/ContactForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+function ContactForm() {
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>이름</Form.Label>
+          <Form.Control type="text" placeholder="이름을 입력해주세요 ~" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formContact">
+          <Form.Label>전화번호</Form.Label>
+          <Form.Control type="" placeholder="전화번호를 입력해주세요 ~" maxLength="11" />
+        </Form.Group>
+        <Button variant="danger" type="submit">
+          연락처 추가
+        </Button>
+      </Form>
+    </div>
+  )
+}
+
+export default ContactForm
